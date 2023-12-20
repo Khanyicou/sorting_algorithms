dc9251d07417329ae83ed12b0dd1dad7871e17ba
Khanyicou <preciouskhanyisa1@gmail.com>
2023-12-19 18:02:29 -0800

    uo

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,52 +1,66 @@
+#include "sort.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "sort.h"
 
 /**
-  * counting_sort - Afunction that sorts an array using counting algorithm.
-  * @array: The array to sort.
-  * @size: size of the array.
-  * Return: N/A
-  */
+ * counting_sort - Sorts an array using the counting sort algorithm.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ * Return: Void.
+ */
 void counting_sort(int *array, size_t size)
 {
-	unsigned int i = 1;
-	int *counter = NULL, k = 0, j = 0;
+	unsigned int i;
+	int *counter = NULL, k = 0, j;
 
 	if (array == NULL || size < 2)
 		return;
 
+	/* Find the maximum value in the array */
 	k = array[0];
-	for (; i < size; i++)
+	for (i = 1; i < size; i++)
 	{
 		if (array[i] > k)
 			k = array[i];
 	}
 
+	/* Allocate memory for the counter array */
 	counter = malloc(sizeof(int) * (k + 1));
 	if (counter == NULL)
 		return;
 
+	/* Initialize counter array to zero */
 	for (j = 0; j <= k; j++)
 		counter[j] = 0;
+
+	/* Count occurrences of each element in the array */
 	for (i = 0; i < size; i++)
 		counter[array[i]] += 1;
-	for (j = 0; j < k; j++)
+
+	/* Update counter array to store cumulative frequencies */
+	for (j = 1; j <= k; j++)
 	{
-		counter[j + 1] += counter[j];
-		printf("%d, ", counter[j]);
+		counter[j] += counter[j - 1];
+		printf("%d, ", counter[j - 1]);
 	}
-	counter[j + 1] += counter[j];
-	printf("%d\n", counter[j + 1]);
-	for (i = 0; i < size; i++)
+	printf("%d\n", counter[k]);
+
+	/* Rearrange elements based on the counter array */
+	for (i = size - 1; i > 0; i--)
 	{
 		j = counter[array[i]] - 1;
-		if (array[i] != array[j])
+		if (i != (size_t)j)
 		{
 			k = array[i];
 			array[i] = array[j];
 			array[j] = k;
 		}
 	}
+
+	/* Print the sorted array */
+	print_array(array, size);
+
+	/* Free allocated memory */
 	free(counter);
 }
+
